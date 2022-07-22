b80d61b7070522d54e82c644d2fcc1e124ca131e
Vince Maina <vchapandrews@gmail.com>
2022-07-22 22:35:34 +0100

    Updating to match previous model structure

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,12 +2,20 @@ DROP TABLE IF EXISTS nx_stations;
 DROP TABLE IF EXISTS nx_journeys;
 
 CREATE TABLE nx_stations (
-  id CHAR(10) PRIMARY KEY,
+  stop_id CHAR(10) PRIMARY KEY,
+  stop_name TEXT NOT NULL,
   is_origin BOOLEAN NOT NULL,
-  is_destination BOOLEAN NOT NULL
-);
+  is_destination BOOLEAN NOT NULL,
+  latitude TEXT,
+  longitude TEXT,
+  address TEXT
+  );
 
 CREATE TABLE nx_journeys (
-  id CHAR(100) PRIMARY KEY,
-  destination CHAR(100) NOT NULL
+  journey_id CHAR(1000) PRIMARY KEY,
+  departure_stop_id CHAR(10) REFERENCES nx_stations NOT NULL,
+  destination_stop_id CHAR(10) REFERENCES nx_stations NOT NULL,
+  fare INTEGER NOT NULL
+  -- departure_time  
+  -- arrival_time
 );
