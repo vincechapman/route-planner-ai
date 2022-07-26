af6aaa3e55b4b64ccb9b86e22393a5d435a97dd1
Vince Maina <vchapandrews@gmail.com>
2022-07-26 10:23:18 +0100

    Commented out the lines that create an 'instance' folder in root (As I'm not it's needed anymore.)

--- route_finder/__init__.py
+++ route_finder/__init__.py
@@ -20,11 +20,11 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     # a simple page that says hello
     @app.route('/hello')
