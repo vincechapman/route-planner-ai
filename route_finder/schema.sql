d4f9596c4c3ed31de2287fc2c1a2135315379efb
Vince Maina <vchapandrews@gmail.com>
2022-07-28 00:51:59 +0100

    Added columns to nx_journeys

--- route_finder/schema.sql
+++ route_finder/schema.sql
@@ -17,11 +17,15 @@ CREATE TABLE nx_stations (
   );
 
 CREATE TABLE nx_journeys (
-  journey_id CHAR(1000) PRIMARY KEY,
-  departure_stop_id CHAR(10) REFERENCES nx_stations NOT NULL,
-  destination_stop_id CHAR(10) REFERENCES nx_stations NOT NULL,
-  fare INTEGER NOT NULL,
-  payload_hash TEXT NOT NULL UNIQUE
-  -- departure_time  
-  -- arrival_time
+  journey_id VARCHAR(500) PRIMARY KEY,
+  departure_stop_id VARCHAR(10) REFERENCES nx_stations NOT NULL,
+  destination_stop_id VARCHAR(10) REFERENCES nx_stations NOT NULL,
+  departure_date DATE NOT NULL,
+  arrival_date DATE NOT NULL,
+  departure_time TIME NOT NULL,
+  arrival_time TIME NOT NULL,
+  price REAL NOT NULL,
+  extra_fees REAL,
+  payload_hash TEXT NOT NULL,
+  request_time TIMESTAMP NOT NULL
 );
