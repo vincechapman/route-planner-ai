39e224fef44d8862ca6b0d7ddd8cc3c22c393d8a
Vince Maina <vchapandrews@gmail.com>
2022-07-27 16:34:36 +0100

    This module handles proxy rotation. We can choose which proxy network we're using simply by changing the environment variables.

---
+++ proxy_rotator/__init__.py
@@ -0,0 +1,51 @@
+def build_rotator(use_proxies=True):
+
+    # ----------------------------------------------------------------------
+    # Importing env vars for bright data
+
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    username = os.environ['BRIGHTDATA_DATACENTER_USERNAME']
+    password = os.environ['BRIGHTDATA_DATACENTER_PASSWORD']
+    location = '-country-gb'
+    port = 22225
+
+
+    # ----------------------------------------------------------------------
+    # Building an opener that uses the proxies provided by bright data.
+    # These proxies will be selected randomly from the current IP selection pool. So the IP should change with each request. If our proxies start getting worn out we can probably build our program so that it automatically refreshes the selection pool using BrightData's API.
+
+    from urllib.request import build_opener, ProxyHandler
+
+    if use_proxies:
+        opener = build_opener(
+            ProxyHandler(
+                {
+                    'http': f'http://{username}{location}:{password}@zproxy.lum-superproxy.io:{port}',
+                    'https': f'http://{username}{location}:{password}@zproxy.lum-superproxy.io:{port}'
+                }
+            )
+        )
+    else:
+        opener = build_opener()
+
+    return opener
+
+
+def bd_proxy_rotator(url, payload=None, headers=None, method=None, opener=build_rotator()):
+
+    from urllib.request import Request
+
+    if payload is not None:
+        import json
+        payload = json.dumps(payload)
+        payload = payload.encode()
+
+    if headers:
+        response = opener.open(Request(url=url, data=payload, headers=headers, method=method))
+    else:
+        response = opener.open(Request(url=url, data=payload, method=method))
+
+    return response
